6c018a387c5d9ba7d8357525c99642eab441e117
Eugene Mathenge <eugenemathenge4@gmail.com>
2025-06-02 15:33:52 -0400

    removed indeclared values

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 import { Mic2, Twitter, Instagram, Github, Linkedin, MessageCircle } from 'lucide-react';
 import { motion } from 'framer-motion';
